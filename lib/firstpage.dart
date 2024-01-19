8b6449d8631493e34d2f6c30da162cc48bece7c7
Sujal <sujalbhaiharikrishanbhai.kaila23@dronafoundation.edu.in>
2024-01-19 15:00:25 +0530

    make working search bar

--- lib/firstpage.dart
+++ lib/firstpage.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:letsplay/p1image.dart';
-import 'package:letsplay/p1groundlist.dart';
 import 'package:letsplay/p1searchbar.dart';
 
 class Myfirstpage extends StatefulWidget {
@@ -18,27 +17,18 @@ class MyfirstpageState extends State<Myfirstpage> {
     return const Scaffold(
       body: Padding(
         padding: EdgeInsets.all(25.0),
-        child: Expanded(
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              p1container(),
-              SizedBox(
-                height: 20,
-              ),
-              p1searchbar(),
-              SizedBox(
-                height: 10,
-              ),
-              Text("Grounds", style: TextStyle(
-                color: Colors.black,
-                fontWeight: FontWeight.w600,
-                fontSize: 20,
-              ),),
-              SizedBox(height: 13,),
-              p1groundlist()
-            ],
-          ),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            p1container(),
+            SizedBox(
+              height: 20,
+            ),
+            p1searchbar(),
+            SizedBox(
+              height: 10,
+            ),
+          ],
         ),
       ),
     );
