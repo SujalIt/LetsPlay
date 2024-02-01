589d227b53809f74aa09579c472e3bf0b23ce112
Sujal <sujalbhaiharikrishanbhai.kaila23@dronafoundation.edu.in>
2024-02-01 11:56:10 +0530

    delete seach sample file

--- lib/searchsample.dart
+++
@@ -1,9 +0,0 @@
-        // FutureBuilder(
-        //   future: ground(),
-        //   builder: (context, AsyncSnapshot<List<LetsPlay>> snapshot) {
-        //     if (!snapshot.hasData) {
-        //       return Center(child: const Text('Loading'));
-        //     } else {
-        //       return result.length == 0
-        //           ? const Center(child: Text("Result not found"))
-        //           : 
