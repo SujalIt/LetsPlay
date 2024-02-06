9bad3ef1a0d26fb0167f868dc54e6c1bbcd65bc9
Sujal <sujalbhaiharikrishanbhai.kaila23@dronafoundation.edu.in>
2024-02-06 12:03:21 +0530

    give padding to datepicker row

--- lib/datepiker.dart
+++ lib/datepiker.dart
@@ -33,66 +33,69 @@ class _DatepikerState extends State<Datepiker> {
   }
   @override
   Widget build(BuildContext context) {
-    return  Row(
-      children: [
-        Flexible(
-          child: Container(
-            height: 42,
-            child: TextField(
-              onTap: date,
-              textAlignVertical: TextAlignVertical.bottom,
-              textAlign: TextAlign.start,
-              keyboardType: TextInputType.none,
-              controller: datecontroller,
-              decoration: InputDecoration(
-                suffixIcon: IconButton(
-                    onPressed: date,
-                    icon: const Icon(Icons.calendar_today)),
-                hintText: DateFormat("dd-MM-yyyy")
-                    .format(currentDate ?? DateTime.now()),
-                enabledBorder: OutlineInputBorder(
-                  borderRadius: BorderRadius.circular(10),
-                  borderSide: const BorderSide(
-                      color: Colors.green, width: 2),
-                ),
-                focusedBorder: OutlineInputBorder(
+    return  Padding(
+      padding: const EdgeInsets.only(left: 8),
+      child: Row(
+        children: [
+          Flexible(
+            child: Container(
+              height: 42,
+              child: TextField(
+                onTap: date,
+                textAlignVertical: TextAlignVertical.bottom,
+                textAlign: TextAlign.start,
+                keyboardType: TextInputType.none,
+                controller: datecontroller,
+                decoration: InputDecoration(
+                  suffixIcon: IconButton(
+                      onPressed: date,
+                      icon: const Icon(Icons.calendar_today)),
+                  hintText: DateFormat("dd-MM-yyyy")
+                      .format(currentDate ?? DateTime.now()),
+                  enabledBorder: OutlineInputBorder(
                     borderRadius: BorderRadius.circular(10),
                     borderSide: const BorderSide(
-                        color: Colors.green, width: 2)),
+                        color: Colors.green, width: 2),
+                  ),
+                  focusedBorder: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(10),
+                      borderSide: const BorderSide(
+                          color: Colors.green, width: 2)),
+                ),
               ),
             ),
           ),
-        ),
-        Padding(
-          padding: const EdgeInsets.all(10.0),
-          child: ElevatedButton(
-              onPressed: () {
-                //2024-01-12 14:45:20.265410
-                DateTime? days = currentDate;
-                receivedDate =
-                    DateFormat("yyyy-MM-dd HH:mm:ss.SSSS")
-                        .format(days!.add(const Duration(days: 1)));
-                currentDate =
-                    DateTime.parse(receivedDate.toString());
-                setState(() {});
-              },
-              style: const ButtonStyle(
-                  shape: MaterialStatePropertyAll(
-                      RoundedRectangleBorder(
-                          borderRadius: BorderRadius.all(
-                              Radius.circular(10)))),
-                  backgroundColor:
-                  MaterialStatePropertyAll(Colors.green)),
-              child: const Text(
-                'Next Day',
-                style: TextStyle(
-                  fontSize: 16,
-                  fontWeight: FontWeight.w500,
-                  color: Colors.white,
-                ),
-              )),
-        ),
-      ],
+          Padding(
+            padding: const EdgeInsets.all(10.0),
+            child: ElevatedButton(
+                onPressed: () {
+                  //2024-01-12 14:45:20.265410
+                  DateTime? days = currentDate;
+                  receivedDate =
+                      DateFormat("yyyy-MM-dd HH:mm:ss.SSSS")
+                          .format(days!.add(const Duration(days: 1)));
+                  currentDate =
+                      DateTime.parse(receivedDate.toString());
+                  setState(() {});
+                },
+                style: const ButtonStyle(
+                    shape: MaterialStatePropertyAll(
+                        RoundedRectangleBorder(
+                            borderRadius: BorderRadius.all(
+                                Radius.circular(10)))),
+                    backgroundColor:
+                    MaterialStatePropertyAll(Colors.green)),
+                child: const Text(
+                  'Next Day',
+                  style: TextStyle(
+                    fontSize: 16,
+                    fontWeight: FontWeight.w500,
+                    color: Colors.white,
+                  ),
+                )),
+          ),
+        ],
+      ),
     );
   }
 }
